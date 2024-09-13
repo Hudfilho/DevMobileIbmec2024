3325a46bd2810da5d4a6ea668e8b3d9da77f3745
Hudfilho <111317360+Hudfilho@users.noreply.github.com>
2024-09-13 10:36:22 -0300

    Update CalculadoraIMC.java
    crime formatado

--- CalculadoraIMC/src/main/java/control/CalculadoraIMC.java
+++ CalculadoraIMC/src/main/java/control/CalculadoraIMC.java
@@ -6,9 +6,9 @@ public class CalculadoraIMC {
         double imc =  peso/(altura*altura);
 
         //Calcular Imc para adultos(Idade>=20 && <=65) Idosos(Idade>=65) e Criancas(Idade<20)
-        if (idade>=20 && idade<=65) {
+        if (idade >= 20 && idade <= 65) {
             return imcAdulto(imc);
-        } else if (idade>65) {
+        } else if (idade > 65) {
             if(sexo.equalsIgnoreCase("masculino")){
                 return imcIdosoMasculino(imc);
             }
@@ -21,49 +21,49 @@ public class CalculadoraIMC {
     }
 
     public String imcAdulto(double imc){
-        if (imc<16){
+        if (imc < 16){
             return "Baixo peso muito grave";
-        } else if (imc<17) {
+        } else if (imc < 17) {
             return "Baixo peso grave";
-        } else if (imc<18.50) {
+        } else if (imc < 18.50) {
             return "Baixo peso";
-        } else if (imc<25) {
+        } else if (imc < 25) {
             return "Peso normal";
-        } else if (imc<30) {
+        } else if (imc < 30) {
             return "Sobrepeso";
-        } else if (imc<35) {
+        } else if (imc < 35) {
             return "Obesidade grau I";
-        } else if (imc<40) {
+        } else if (imc < 40) {
             return "Obesidade grau II";
         }
         return "Obesidade grau III (obesidade mórbida)";
     }
 
     private String imcIdosoMasculino(double imc) {
-        if (imc<22){
+        if (imc < 22){
             return "Baixo peso";
-        } else if (imc<27.1) {
+        } else if (imc < 27.1) {
             return "Peso normal";
-        } else if (imc<30.1) {
+        } else if (imc < 30.1) {
             return "Sobrepeso";
-        } else if (imc<35.1) {
+        } else if (imc < 35.1) {
             return "Obesidade grau I";
-        } else if (imc<40) {
+        } else if (imc < 40) {
             return "Obesidade grau II";
         }
         return "Obesidade grau III (obesidade mórbida)";
     }
 
     private String imcIdosoFeminino(double imc) {
-        if (imc<22){
+        if (imc < 22){
             return "Baixo peso";
-        } else if (imc<27.1) {
+        } else if (imc < 27.1) {
             return "Peso normal";
-        } else if (imc<32.1) {
+        } else if (imc < 32.1) {
             return "Sobrepeso";
-        } else if (imc<37.1) {
+        } else if (imc < 37.1) {
             return "Obesidade grau I";
-        } else if (imc<42) {
+        } else if (imc < 42) {
             return "Obesidade grau II";
         }
         return "Obesidade grau III (obesidade mórbida)";
