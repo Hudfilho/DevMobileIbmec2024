0a376fbacc76f0a37d4aa0d318e7480746fb6187
Hudfilho <111317360+Hudfilho@users.noreply.github.com>
2024-09-13 09:09:38 -0300

    Calculadora V1.2
    Ajustes nos casos de teste

--- CalculadoraIMC/src/test/java/teste/CalculadoraIMCTeste.java
+++ CalculadoraIMC/src/test/java/teste/CalculadoraIMCTeste.java
@@ -15,38 +15,56 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcAdultoBaixoPesoGrave() {
+    public void testeImcAdultoBaixoPesoGraveLimInferior() {
         assertEquals("Baixo peso grave", calculadora.calcularImc(46.24, 1.70, 30, "masculino"));
+    }
+    @Test
+    public void testeImcAdultoBaixoPesoGraveLimSuperior() {
         assertEquals("Baixo peso grave", calculadora.calcularImc(49.10, 1.70, 30, "masculino"));
     }
 
     @Test
-    public void testeImcAdultoBaixoPeso() {
+    public void testeImcAdultoBaixoPesoLimInferior() {
         assertEquals("Baixo peso", calculadora.calcularImc(49.15, 1.70, 30, "masculino"));
+    }
+    @Test
+    public void testeImcAdultoBaixoPesoLimSuperior() {
         assertEquals("Baixo peso", calculadora.calcularImc(53.45, 1.70, 30, "masculino"));
     }
 
     @Test
-    public void testeImcAdultoPesoNormal() {
+    public void testeImcAdultoPesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(53.47, 1.70, 30, "masculino"));
+    }
+    @Test
+    public void testeImcAdultoPesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(72.23, 1.70, 30, "masculino"));
     }
 
     @Test
-    public void testeImcAdultoSobrepeso() {
+    public void testeImcAdultoSobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(72.25, 1.70, 30, "masculino"));
+    }
+    @Test
+    public void testeImcAdultoSobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(86.65, 1.70, 30, "masculino"));
     }
 
     @Test
-    public void testeImcAdultoObesidadeGrauI() {
+    public void testeImcAdultoObesidadeGrauILimInferior() {
         assertEquals("Obesidade grau I", calculadora.calcularImc(86.7, 1.70, 30, "masculino"));
+    }
+    @Test
+    public void testeImcAdultoObesidadeGrauILimSuperior() {
         assertEquals("Obesidade grau I", calculadora.calcularImc(101.12, 1.70, 30, "masculino"));
     }
 
     @Test
-    public void testeImcAdultoObesidadeGrauII() {
+    public void testeImcAdultoObesidadeGrauIILimInferior() {
         assertEquals("Obesidade grau II", calculadora.calcularImc(101.15, 1.70, 30, "masculino"));
+    }
+    @Test
+    public void testeImcAdultoObesidadeGrauII() {
         assertEquals("Obesidade grau II", calculadora.calcularImc(115.57, 1.70, 30, "masculino"));
     }
 
@@ -63,27 +81,45 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcIdosoMasculinoPesoNormal() {
+    public void testeImcIdosoMasculinoPesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(61.36, 1.67, 70, "masculino"));
-        assertEquals("Peso normal", calculadora.calcularImc(75.57, 1.67, 70, "masculino"));
     }
 
     @Test
-    public void testeImcIdosoMasculinoSobrepeso() {
+    public void testeImcIdosoMasculinoPesoNormalLimSuperior() {
+        assertEquals("Peso normal", calculadora.calcularImc(75.57, 1.67, 70, "masculino"));
+    }
+
+
+    @Test
+    public void testeImcIdosoMasculinoSobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(75.58, 1.67, 70, "masculino"));
+    }
+
+    @Test
+    public void testeImcIdosoMasculinoSobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(83.94, 1.67, 70, "masculino"));
     }
 
     @Test
-    public void testeImcIdosoMasculinoObesidadeGrauI() {
+    public void testeImcIdosoMasculinoObesidadeGrauILimInferior() {
         assertEquals("Obesidade grau I", calculadora.calcularImc(83.95, 1.67, 70, "masculino"));
-        assertEquals("Obesidade grau I", calculadora.calcularImc(97.89, 1.67, 70, "masculino"));
     }
 
     @Test
-    public void testeImcIdosoMasculinoObesidadeGrauII() {
-        assertEquals("Obesidade grau II", calculadora.calcularImc(97.9, 1.67, 70, "masculino"));
-        assertEquals("Obesidade grau II", calculadora.calcularImc(111.5, 1.67, 70, "masculino"));
+    public void testeImcIdosoMasculinoObesidadeGrauILimSuperior() {
+        assertEquals("Obesidade grau I", calculadora.calcularImc(97.89, 1.67, 70, "masculino"));
+    }
+
+
+    @Test
+    public void testeImcIdosoMasculinoObesidadeGrauIILimInferior() {
+        assertEquals("Obesidade grau II", calculadora.calcularImc(97.90, 1.67, 70, "masculino"));
+    }
+
+    @Test
+    public void testeImcIdosoMasculinoObesidadeGrauIILimSuperior() {
+        assertEquals("Obesidade grau II", calculadora.calcularImc(111.50, 1.67, 70, "masculino"));
     }
 
     @Test
@@ -98,26 +134,42 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcIdosoFemininoPesoNormal() {
+    public void testeImcIdosoFemininoPesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(56.33, 1.60, 70, "feminino"));
-        assertEquals("Peso normal", calculadora.calcularImc(69.2, 1.60, 70, "feminino"));
     }
 
     @Test
-    public void testeImcIdosoFemininoSobrepeso() {
-        assertEquals("Sobrepeso", calculadora.calcularImc(69.5, 1.60, 70, "feminino"));
+    public void testeImcIdosoFemininoPesoNormalLimSuperior() {
+        assertEquals("Peso normal", calculadora.calcularImc(69.20, 1.60, 70, "feminino"));
+    }
+
+    @Test
+    public void testeImcIdosoFemininoSobrepesoLimInferior() {
+        assertEquals("Sobrepeso", calculadora.calcularImc(69.50, 1.60, 70, "feminino"));
+    }
+
+    @Test
+    public void testeImcIdosoFemininoSobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(82.17, 1.60, 70, "feminino"));
     }
 
     @Test
-    public void testeImcIdosoFemininoObesidadeGrauI() {
+    public void testeImcIdosoFemininoObesidadeGrauILimInferior() {
         assertEquals("Obesidade grau I", calculadora.calcularImc(82.18, 1.60, 70, "feminino"));
+    }
+
+    @Test
+    public void testeImcIdosoFemininoObesidadeGrauILimSuperior() {
         assertEquals("Obesidade grau I", calculadora.calcularImc(92.97, 1.60, 70, "feminino"));
     }
 
     @Test
-    public void testeImcIdosoFemininoObesidadeGrauII() {
+    public void testeImcIdosoFemininoObesidadeGrauIILimInferior() {
         assertEquals("Obesidade grau II", calculadora.calcularImc(94.98, 1.60, 70, "feminino"));
+    }
+
+    @Test
+    public void testeImcIdosoFemininoObesidadeGrauIILimSuperior() {
         assertEquals("Obesidade grau II", calculadora.calcularImc(107.52, 1.60, 70, "feminino"));
     }
 
@@ -133,14 +185,22 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcMenino2PesoNormal() {
+    public void testeImcMenino2PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(10.7, 0.85, 2, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino2PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(13.14, 0.85, 2, "masculino"));
     }
 
     @Test
-    public void testeImcMenino2Sobrepeso() {
+    public void testeImcMenino2SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(13.15, 0.85, 2, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino2SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(13.87, 0.85, 2, "masculino"));
     }
 
@@ -156,14 +216,22 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcMenino4PesoNormal() {
+    public void testeImcMenino4PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(12.64, 0.95, 4, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino4PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(15.34, 0.95, 4, "masculino"));
     }
 
     @Test
-    public void testeImcMenino4Sobrepeso() {
+    public void testeImcMenino4SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(15.35, 0.95, 4, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino4SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(16.15, 0.95, 4, "masculino"));
     }
 
@@ -179,14 +247,22 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcMenino6PesoNormal() {
+    public void testeImcMenino6PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(18.26, 1.15, 6, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino6PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(22.48, 1.15, 6, "masculino"));
     }
 
     @Test
-    public void testeImcMenino6Sobrepeso() {
+    public void testeImcMenino6SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(22.49, 1.15, 6, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino6SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(24.33, 1.15, 6, "masculino"));
     }
 
@@ -202,15 +278,23 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcMenino8PesoNormal() {
+    public void testeImcMenino8PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(23.33, 1.30, 8, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino8PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(30.41, 1.30, 8, "masculino"));
     }
 
     @Test
-    public void testeImcMenino8Sobrepeso() {
+    public void testeImcMenino8SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(30.42, 1.30, 8, "masculino"));
-        assertEquals("Sobrepeso", calculadora.calcularImc(33.8, 1.30, 8, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino8SobrepesoLimSuperior() {
+        assertEquals("Sobrepeso", calculadora.calcularImc(33.80, 1.30, 8, "masculino"));
     }
 
     @Test
@@ -225,14 +309,22 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcMenino10PesoNormal() {
+    public void testeImcMenino10PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(29.86, 1.45, 10, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino10PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(40.36, 1.45, 10, "masculino"));
     }
 
     @Test
-    public void testeImcMenino10Sobrepeso() {
+    public void testeImcMenino10SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(40.37, 1.45, 10, "masculino"));
+    }
+
+    @Test
+    public void testeImcMenino10SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(46.25, 1.45, 10, "masculino"));
     }
 
@@ -249,14 +341,22 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcMenina2PesoNormal() {
+    public void testeImcMenina2PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(10.5, 0.85, 2, "feminino"));
-        assertEquals("Peso normal", calculadora.calcularImc(13, 0.85, 2, "feminino"));
     }
 
     @Test
-    public void testeImcMenina2Sobrepeso() {
+    public void testeImcMenina2PesoNormalLimSuperior() {
+        assertEquals("Peso normal", calculadora.calcularImc(13.0, 0.85, 2, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina2SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(13.1, 0.85, 2, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina2SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(13.79, 0.85, 2, "feminino"));
     }
 
@@ -272,14 +372,22 @@ public class CalculadoraIMCTeste {
     }
 
     @Test
-    public void testeImcMenina4PesoNormal() {
+    public void testeImcMenina4PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(12.46, 0.95, 4, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina4PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(15.16, 0.95, 4, "feminino"));
     }
 
     @Test
-    public void testeImcMenina4Sobrepeso() {
+    public void testeImcMenina4SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(15.17, 0.95, 4, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina4SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(16.24, 0.95, 4, "feminino"));
     }
 
@@ -288,20 +396,29 @@ public class CalculadoraIMCTeste {
         assertEquals("Obesidade", calculadora.calcularImc(16.25, 0.95, 4, "feminino"));
     }
 
+    //Menina 6anos
     @Test
     public void testeImcMenina6BaixoPeso() {
         assertEquals("Baixo peso", calculadora.calcularImc(17.72, 1.15, 6, "feminino"));
     }
 
     @Test
-    public void testeImcMenina6PesoNormal() {
+    public void testeImcMenina6PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(17.73, 1.15, 6, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina6PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(22.48, 1.15, 6, "feminino"));
     }
 
     @Test
-    public void testeImcMenina6Sobrepeso() {
+    public void testeImcMenina6SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(22.49, 1.15, 6, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina6SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(24.86, 1.15, 6, "feminino"));
     }
 
@@ -310,20 +427,29 @@ public class CalculadoraIMCTeste {
         assertEquals("Obesidade", calculadora.calcularImc(24.87, 1.15, 6, "feminino"));
     }
 
+    //Menina 8anos
     @Test
     public void testeImcMenina8BaixoPeso() {
         assertEquals("Baixo peso", calculadora.calcularImc(22.98, 1.30, 8, "feminino"));
     }
 
     @Test
-    public void testeImcMenina8PesoNormal() {
+    public void testeImcMenina8PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(22.99, 1.30, 8, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina8PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(30.75, 1.30, 8, "feminino"));
     }
 
     @Test
-    public void testeImcMenina8Sobrepeso() {
+    public void testeImcMenina8SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(30.76, 1.30, 8, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina8SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(34.81, 1.30, 8, "feminino"));
     }
 
@@ -332,20 +458,29 @@ public class CalculadoraIMCTeste {
         assertEquals("Obesidade", calculadora.calcularImc(34.82, 1.30, 8, "feminino"));
     }
 
+    //Menina 10anos
     @Test
     public void testeImcMenina10BaixoPeso() {
         assertEquals("Baixo peso", calculadora.calcularImc(29.43, 1.45, 10, "feminino"));
     }
 
     @Test
-    public void testeImcMenina10PesoNormal() {
+    public void testeImcMenina10PesoNormalLimInferior() {
         assertEquals("Peso normal", calculadora.calcularImc(29.44, 1.45, 10, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina10PesoNormalLimSuperior() {
         assertEquals("Peso normal", calculadora.calcularImc(42.04, 1.45, 10, "feminino"));
     }
 
     @Test
-    public void testeImcMenina10Sobrepeso() {
+    public void testeImcMenina10SobrepesoLimInferior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(42.05, 1.45, 10, "feminino"));
+    }
+
+    @Test
+    public void testeImcMenina10SobrepesoLimSuperior() {
         assertEquals("Sobrepeso", calculadora.calcularImc(48.35, 1.45, 10, "feminino"));
     }
 
